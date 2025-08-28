4c0903edb02501c51dc18d68bfa030a3ba5c88fb
Anton Evangelatov <anton.evangelatov@oplabs.co>
2025-08-28 18:41:49 +0200

    try different block numbers

--- op-acceptance-tests/tests/sync_tester_ext_el/init_test.go
+++ op-acceptance-tests/tests/sync_tester_ext_el/init_test.go
@@ -9,15 +9,16 @@ import (
 )
 
 func TestMain(m *testing.M) {
+	L2NetworkName := "sepolia"
 	L2ELEndpoint := "https://sepolia.optimism.io/"
 	L1CLBeaconEndpoint := "https://beacon-api-proxy-sepolia.primary.client.dev.oplabs.cloud"
 	L1ELEndpoint := "https://proxyd-l1-sepolia.primary.client.dev.oplabs.cloud"
 	L1ChainID := eth.ChainIDFromUInt64(11155111)
 
-	presets.DoMain(m, presets.WithMinimalExternalELWithSuperchainRegistry(L1CLBeaconEndpoint, L1ELEndpoint, L2ELEndpoint, L1ChainID, "sepolia", eth.FCUState{
-		Latest:    22285447,
-		Safe:      22285447,
-		Finalized: 22285447,
+	presets.DoMain(m, presets.WithMinimalExternalELWithSuperchainRegistry(L1CLBeaconEndpoint, L1ELEndpoint, L2ELEndpoint, L1ChainID, L2NetworkName, eth.FCUState{
+		Latest:    31987000,
+		Safe:      31987000,
+		Finalized: 31987000,
 	}),
 		presets.WithCompatibleTypes(compat.SysGo),
 	)
